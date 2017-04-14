6055352dc0b3536e5ba522ab1d874ec11488b784
Scott Fox <scottfox5@gmail.com>
2017-04-13 22:05:05 -0500

    modal carousel and close

--- public/scripts/carController.js
+++ public/scripts/carController.js
@@ -1,26 +1,20 @@
-afPhoto.controller('CarController', function ($scope) {
+afPhoto.controller('CarController', function ($scope, $uibModal, $uibModalInstance) {
   console.log('Car Controller connected...')
   console.log ('modal stuff... this', this)
+
+  // closing the modal on button ng-click='cancel()'
+  $scope.closeModal = function () {
+    console.log('Close modal:')
+    $uibModalInstance.dismiss('button click');
+  };
 
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
-  var slides = $scope.slides = [];
   var currIndex = 0;
-
-
-  // $scope.addSlide = function() {
-  //   var newWidth = 600 + slides.length + 1;
-  //   slides.push({
-  //     image: '//unsplash.it/' + newWidth + '/300',
-  //     text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-  //     id: currIndex++
-  //   });
-  // };
-
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
+  var modalInstance = undefined;
+  var slides = $scope.slides = [
+    {
       image: 'public/assets/photos/adriennefox.photography/maternal-instinct/IMG_1634.jpg',
       text: 'maternal-instinct-pumping',
       id: currIndex++
@@ -30,57 +24,6 @@ afPhoto.controller('CarController', function ($scope) {
       text: 'maternal-instinct-julia',
       id: currIndex++
     }
-    );
-  };
-
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
-  for (var i = 0; i < 4; i++) {
-    $scope.addSlide();
-  }
-
-  // Randomize logic below
-
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
-
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
-
-  // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp, current, top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
-
-
-
-  // END of carousel stuff
-
-
-
-
-
+  ];
 
 });
